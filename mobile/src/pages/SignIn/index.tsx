2bfcc9830d030a06c4f41187cc747aa0202ba62c
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2020-11-11 09:32:47 -0300

    [Mobile] Add options Forgot Password and Create Account on authentication screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          contentContainerStyle={{flex: 1}}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+            <Title>Faça seu logon</Title>
 
-      <Input placeholder="Email" name="email" icon="mail" />
-      <Input placeholder="Password" name="password" icon="lock" />
+            <Input placeholder="Email" name="email" icon="mail" />
+            <Input placeholder="Password" name="password" icon="lock" />
 
-      <Button>Enter</Button>
-    </Container>
+            <Button>Enter</Button>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Forgot password</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create Account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+
+
   );
 };
 
