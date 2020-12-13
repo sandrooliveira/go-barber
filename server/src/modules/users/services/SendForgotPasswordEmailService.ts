a42d18c4637c4bc0678f47032aec628b978811dd
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2020-12-13 20:39:20 -0300

    [Server] Add unit test to make sure email was sent using TDD

---
+++ server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUserRepository from '../repositories/IUserRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
