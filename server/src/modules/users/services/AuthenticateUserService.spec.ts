a1503a28e6f8b8c8a9b31a72cab81e977184cca4
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2020-12-13 14:22:58 -0300

    [Server] Add unit tests to AuthenticateUserService
    Create separated service for hash handling

--- server/src/modules/users/services/AuthenticateUserService.spec.ts
+++ server/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -2,6 +2,7 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserServices';
 import CreateUserService from './CreateUserService';
+import AppError from '../../../shared/errors/AppError';
 
 describe('Authenticate User', () => {
   it('should be able to authenticate', async () => {
@@ -29,5 +30,48 @@ describe('Authenticate User', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const response = authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    await createUserService.execute({
+      name: 'Jhon Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: 'wrong-password',
+    });
+
+    expect(response).rejects.toBeInstanceOf(AppError);
   });
 });
