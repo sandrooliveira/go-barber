f9f7f4b32f5d47359afda263add72823d3a05c27
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2020-12-25 11:06:39 -0300

    [Server] Add email template structure using Handlebars

--- server/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ server/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,10 +1,17 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { inject, injectable } from 'tsyringe';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private MailTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       this.client = nodemailer.createTransport({
         host: account.smtp.host,
@@ -18,12 +25,23 @@ export default class EtherealMailProvider implements IMailProvider {
     });
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    subject,
+    from,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.MailTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
