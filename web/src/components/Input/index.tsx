8531c6ef6221ea03e3cfeb1ff925775edcc501b8
Sandro Augusto de Oliveira <sandroaugusto67@gmail.com>
2021-01-04 09:15:38 -0300

    [Web] Add profile page
    - Add possibility to change user avatar
    - Add possibility to change user name and email
    - Add possibility to change password

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -14,9 +14,15 @@ import { Container, Error } from './styles';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon: React.ComponentType<IconBaseProps>;
+  containerStyle?: object;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  containerStyle = {},
+  icon: Icon,
+  ...rest
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setFocus] = useState(false);
   const [isFilled, setFilled] = useState(false);
@@ -42,7 +48,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
---
+++ web/src/pages/Profile/index.tsx
@@ -0,0 +1,190 @@
+import React, { ChangeEvent, useCallback, useRef } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import api from '../../services/api';
+
+import { Container, Content, AvatarInput } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { useToast } from '../../hooks/toast';
+
+import getErrorInfo from '../../utils/getErrorInfo';
+import { useAuth } from '../../hooks/auth';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  old_password: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const { user, updateUser } = useAuth();
+
+  const onSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Name is mandatory'),
+          email: Yup.string()
+            .required('E-mail is mandatory')
+            .email('Not valid email'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: val => !!val.length,
+            then: Yup.string().required('Required field'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: val => !!val.length,
+              then: Yup.string().required('Required field'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password')], 'Password confirmation is wrong'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
+
+        history.push('/dashboard');
+
+        addToast({
+          type: 'success',
+          title: 'Profile successfully updated',
+          description:
+            'Your profile information has been successfully updated!',
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getErrorInfo(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Profile update error',
+          description: 'There was an issue on profile update, please try again',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData();
+
+        data.append('avatar', e.target.files[0]);
+
+        api.patch('/users/avatar', data).then(response => {
+          updateUser(response.data);
+
+          addToast({
+            type: 'success',
+            title: 'Avatar updated',
+          });
+        });
+      }
+    },
+    [addToast, updateUser],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+          onSubmit={onSubmit}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <label htmlFor="avatar">
+              <FiCamera />
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
+          </AvatarInput>
+
+          <h1>My Profile</h1>
+
+          <Input icon={FiUser} name="name" placeholder="Name" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            icon={FiLock}
+            name="old_password"
+            type="password"
+            placeholder="Current Password"
+          />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="New Password"
+          />
+          <Input
+            icon={FiLock}
+            name="password_confirmation"
+            type="password"
+            placeholder="Password Confirmation"
+          />
+          <Button type="submit">Confirm Changes</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
